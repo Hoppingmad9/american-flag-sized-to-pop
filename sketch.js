1f59b15a7da2a3082e89e9ba0ebf02d5aeea7d71
Sam Dean <samdena09@gmail.com>
2021-01-12 12:20:06 +0000

    negative star size bug fix order data

--- sketch.js
+++ sketch.js
@@ -16,74 +16,57 @@ function setup() {
 	star_outer_rad = stripe_height*4/5/2
 	star_inner_rad = star_outer_rad*30/70
 
-	state_pops = [	['Alabama',4888949],
-					['Alaska',738068],
-					['Arizona',7123898],
-					['Arkansas',3020327],
-					['California',39776830],
-					['Colorado',5684203],
-					['Connecticut',3588683],
-					['Delaware',971180],
-					['Florida',21312211],
-					['Georgia',10545138],
-					['Hawaii',1426393],
-					['Idaho',1753860],
-					['Illinois',12768320],
-					['Indiana',6699629],
-					['Iowa',3160553],
-					['Kansas',2918515],
-					['Kentucky',4472265],
-					['Louisiana',4682509],
-					['Maine',1341582],
-					['Maryland',6079602],
-					['Massachusetts',6895917],
-					['Michigan',9991177],
-					['Minnesota',5628162],
-					['Mississippi',2982785],
-					['Missouri',6135888],
-					['Montana',1062330],
-					['Nebraska',1932549],
-					['Nevada',3056824],
-					['New Hampshire',1350575],
-					['New Jersey',9032872],
-					['New Mexico',2090708],
-					['New York',19862512],
-					['North Carolina',10390149],
-					['North Dakota',755238],
-					['Ohio',11694664],
-					['Oklahoma',3940521],
-					['Oregon',4199563],
-					['Pennsylvania',12823989],
-					['Rhode Island',1061712],
-					['South Carolina',5088916],
-					['South Dakota',877790],
-					['Tennessee',6782564],
-					['Texas',28704330],
-					['Utah',3159345],
-					['Vermont',623960],
-					['Virginia',8525660],
-					['Washington',7530552],
-					['West Virginia',1803077],
-					['Wisconsin',5818049],
-					['Wyoming',573720]]
-
-	largest_pop = 0
-
-	for (let i = 0; i < state_pops.length; i++) {
-		if ((state_pops[i][1]) > largest_pop) {
-			largest_pop = state_pops[i][1]
-		}
-	}
-
-	star_scalers = []
-	star_scaling = 2.7
-
-	largest_pop = Math.log(largest_pop/100000)/Math.log(10)
-
-	for (let i = 0; i < state_pops.length; i++) {
-		temp_pop = Math.log(state_pops[i][1]/1000000)/Math.log(10)
-		star_scalers.push(temp_pop/largest_pop*star_scaling)
-	}
+	state_pops = [	['Alabama',4888949,22],
+				['Alaska',738068,49],
+				['Arizona',7123898,48],
+				['Arkansas',3020327,25],
+				['California',39776830,31],
+				['Colorado',5684203,38],
+				['Connecticut',3588683,5],
+				['Delaware',971180,1],
+				['Florida',21312211,27],
+				['Georgia',10545138,4],
+				['Hawaii',1426393,50],
+				['Idaho',1753860,43],
+				['Illinois',12768320,21],
+				['Indiana',6699629,19],
+				['Iowa',3160553,29],
+				['Kansas',2918515,34],
+				['Kentucky',4472265,15],
+				['Louisiana',4682509,18],
+				['Maine',1341582,23],
+				['Maryland',6079602,7],
+				['Massachusetts',6895917,6],
+				['Michigan',9991177,26],
+				['Minnesota',5628162,32],
+				['Mississippi',2982785,20],
+				['Missouri',6135888,24],
+				['Montana',1062330,41],
+				['Nebraska',1932549,37],
+				['Nevada',3056824,36],
+				['New Hampshire',1350575,9],
+				['New Jersey',9032872,3],
+				['New Mexico',2090708,47],
+				['New York',19862512,11],
+				['North Carolina',10390149,12],
+				['North Dakota',755238,39],
+				['Ohio',11694664,17],
+				['Oklahoma',3940521,46],
+				['Oregon',4199563,33],
+				['Pennsylvania',12823989,2],
+				['Rhode Island',1061712,13],
+				['South Carolina',5088916,8],
+				['South Dakota',877790,40],
+				['Tennessee',6782564,16],
+				['Texas',28704330,28],
+				['Utah',3159345,45],
+				['Vermont',623960,14],
+				['Virginia',8525660,10],
+				['Washington',7530552,42],
+				['West Virginia',1803077,35],
+				['Wisconsin',5818049,30],
+				['Wyoming',573720,44],
+			]
 
 	original_states = [[6, 'Connecticut'],
 	[7, 'Delaware'],
@@ -114,6 +97,50 @@ function setup() {
 			stripe_heights.push([state_pops[original_states[i][0]][1]/orig_states_total_pop*flag_height, stripe_heights[i-1][0]+stripe_heights[i-1][1]])
 		}
 	}
+
+	star_order = 2 // 1 alphabetical, 2 population, 3 order of admittance
+
+	if (star_order == 1) {
+		state_pops.sort(compareFirstColumn);
+	} else if (star_order == 2) {
+		state_pops.sort(compareSecondColumn);
+	} else if (star_order == 3) {
+		state_pops.sort(compareThirdColumn);
+	}
+
+	console.log(state_pops)
+
+	largest_pop = 0
+	smallest_pop = 9999999999999
+
+	for (let i = 0; i < state_pops.length; i++) {
+		if ((state_pops[i][1]) > largest_pop) {
+			largest_pop = state_pops[i][1]
+		}
+		if ((state_pops[i][1]) < smallest_pop) {
+			smallest_pop = state_pops[i][1]
+		}
+	}
+
+	console.log(smallest_pop)
+
+	star_scalers = []
+	star_scaling = 1.7
+	smallest_star = 0.05
+
+	if (false) {
+		// show negative star size // BUG:
+		smallest_pop = 1000000
+	}
+
+	largest_pop = Math.log(largest_pop/smallest_pop)/Math.log(10)
+
+	for (let i = 0; i < state_pops.length; i++) {
+		temp_pop = Math.log(state_pops[i][1]/smallest_pop)/Math.log(10)
+		star_scalers.push(temp_pop/largest_pop*(star_scaling-smallest_star)+smallest_star)
+	}
+
+	console.log(star_scalers)
 
 	createCanvas(flag_width, flag_height);
 	background(0,0,0);
@@ -162,6 +189,32 @@ function setup() {
 
 }
 
+function compareThirdColumn(a, b) {
+    if (a[2] === b[2]) {
+        return 0;
+    }
+    else {
+        return (a[2] < b[2]) ? -1 : 1;
+    }
+}
+
+function compareSecondColumn(a, b) {
+    if (a[1] === b[1]) {
+        return 0;
+    }
+    else {
+        return (a[1] < b[1]) ? -1 : 1;
+    }
+}
+
+function compareFirstColumn(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
